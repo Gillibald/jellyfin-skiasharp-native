7b3dd046c0b925e7c252953925bea50a1716474f
Benedikt Schroeder <benedikt.schroeder@outlook.de>
2020-05-15 10:55:21 +0200

    Copy all content to the temp folder

--- Jellyfin.SkiaSharp.NativeAssets.LinuxArm/build-in-docker.sh
+++ Jellyfin.SkiaSharp.NativeAssets.LinuxArm/build-in-docker.sh
@@ -3,5 +3,5 @@
 docker build -t libskiasharp-linuxarm .
 mkdir -p ./runtimes/linux-arm/native
 mkdir -p ./runtimes/linux-arm64/native
-docker run --rm -v "./runtimes/linux-arm/native:/temp" "libskiasharp-linuxarm" cp /build/skia/out/linux-arm/libSkiaSharp.so /temp/
-docker run --rm -v "./runtimes/linux-arm64/native:/temp64" "libskiasharp-linuxarm" cp /build/skia/out/linux-arm64/libSkiaSharp.so /temp64/
+docker run --rm -v "./runtimes/linux-arm/native:/temp" "libskiasharp-linuxarm" cp /build/ /temp/
+docker run --rm -v "./runtimes/linux-arm64/native:/temp64" "libskiasharp-linuxarm" cp /build/ /temp64/
